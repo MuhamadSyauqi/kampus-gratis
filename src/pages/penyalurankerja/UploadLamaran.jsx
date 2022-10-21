72893873cf350d1e3d55bac213c959f4edfb9eb7
Abdul11201 <hafidh.sungkar11@gmail.com>
2022-10-21 09:07:42 +0700

    class -> className

--- src/pages/penyalurankerja/UploadLamaran.jsx
+++ src/pages/penyalurankerja/UploadLamaran.jsx
@@ -8,95 +8,95 @@ const UploadLamaran = () => {
             <Header4 />
             <section className="py-5">
                 <div className="container card shadow my-5 p-4">
-                    <form class="row g-3">
+                    <form className="row g-3">
 
 
-                        <div class="col-12">
-                            <div class="row g-xl-0 align-items-center">
-                                <div class="col-lg-4">
-                                    <h6 class="mb-lg-0">Nama Lengkap <span class="text-danger">*</span></h6>
+                        <div className="col-12">
+                            <div className="row g-xl-0 align-items-center">
+                                <div className="col-lg-4">
+                                    <h6 className="mb-lg-0">Nama Lengkap <span className="text-danger">*</span></h6>
                                 </div>
-                                <div class="col-lg-8">
-                                    <input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap" />
+                                <div className="col-lg-8">
+                                    <input type="text" className="form-control" id="nama_lengkap" name="nama_lengkap" />
                                 </div>
                             </div>
                         </div>
 
 
-                        <div class="col-12">
-                            <div class="row g-xl-0 align-items-center">
-                                <div class="col-lg-4">
-                                    <h6 class="mb-lg-0">Email <span class="text-danger">*</span></h6>
+                        <div className="col-12">
+                            <div className="row g-xl-0 align-items-center">
+                                <div className="col-lg-4">
+                                    <h6 className="mb-lg-0">Email <span className="text-danger">*</span></h6>
                                 </div>
-                                <div class="col-lg-8">
-                                    <input type="email" class="form-control" id="email" name="email" />
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="col-12">
-                            <div class="row g-xl-0 align-items-center">
-                                <div class="col-lg-4">
-                                    <h6 class="mb-lg-0">Phone <span class="text-danger">*</span></h6>
-                                </div>
-                                <div class="col-lg-8">
-                                    <input type="text" class="form-control" id="phone" name="phone" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-12">
-                            <div class="row g-xl-0 align-items-center">
-                                <div class="col-lg-4">
-                                    <h6 class="mb-lg-0">Perusahaan sebelumnya <span class="text-danger">*</span></h6>
-                                </div>
-                                <div class="col-lg-8">
-                                    <input type="text" class="form-control" id="perusahaan_sebelumnya" name="perusahaan_sebelumnya" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-12">
-                            <div class="row g-xl-0 align-items-center">
-                                <div class="col-lg-4">
-                                    <h6 class="mb-lg-0">linkedin Url<span class="text-danger">*</span></h6>
-                                </div>
-                                <div class="col-lg-8">
-                                    <input type="text" class="form-control" id="linkedin_url" name="linkedin_url" />
+                                <div className="col-lg-8">
+                                    <input type="email" className="form-control" id="email" name="email" />
                                 </div>
                             </div>
                         </div>
 
 
-
-                        <div class="col-12">
-                            <div class="row g-xl-0">
-                                <div class="col-lg-4">
-                                    <h6 class="mb-lg-0">Informasi Tambahan <span class="text-danger">*</span></h6>
+                        <div className="col-12">
+                            <div className="row g-xl-0 align-items-center">
+                                <div className="col-lg-4">
+                                    <h6 className="mb-lg-0">Phone <span className="text-danger">*</span></h6>
                                 </div>
-                                <div class="col-lg-8">
-                                    <textarea class="form-control" rows="3" placeholder="" name="informasi_tambahan"></textarea>
+                                <div className="col-lg-8">
+                                    <input type="text" className="form-control" id="phone" name="phone" />
                                 </div>
                             </div>
                         </div>
 
-                        <div class="col-12">
-                            <div class="row g-xl-0 align-items-center">
-                                <div class="col-lg-4">
-                                    <h6 class="mb-lg-0">Resume/CV<span class="text-danger">*</span></h6>
+                        <div className="col-12">
+                            <div className="row g-xl-0 align-items-center">
+                                <div className="col-lg-4">
+                                    <h6 className="mb-lg-0">Perusahaan sebelumnya <span className="text-danger">*</span></h6>
                                 </div>
-                                <div class="col-lg-8">
-                                    <input type="file" class="form-control" id="resume" name="resume" />
+                                <div className="col-lg-8">
+                                    <input type="text" className="form-control" id="perusahaan_sebelumnya" name="perusahaan_sebelumnya" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12">
+                            <div className="row g-xl-0 align-items-center">
+                                <div className="col-lg-4">
+                                    <h6 className="mb-lg-0">linkedin Url<span className="text-danger">*</span></h6>
+                                </div>
+                                <div className="col-lg-8">
+                                    <input type="text" className="form-control" id="linkedin_url" name="linkedin_url" />
                                 </div>
                             </div>
                         </div>
 
 
 
+                        <div className="col-12">
+                            <div className="row g-xl-0">
+                                <div className="col-lg-4">
+                                    <h6 className="mb-lg-0">Informasi Tambahan <span className="text-danger">*</span></h6>
+                                </div>
+                                <div className="col-lg-8">
+                                    <textarea className="form-control" rows="3" placeholder="" name="informasi_tambahan"></textarea>
+                                </div>
+                            </div>
+                        </div>
 
-                        <div class="col-12 text-sm-end">
-                            <button class="btn btn-primary mb-0">Konfirmasi lamaran</button>
+                        <div className="col-12">
+                            <div className="row g-xl-0 align-items-center">
+                                <div className="col-lg-4">
+                                    <h6 className="mb-lg-0">Resume/CV<span className="text-danger">*</span></h6>
+                                </div>
+                                <div className="col-lg-8">
+                                    <input type="file" className="form-control" id="resume" name="resume" />
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                        <div className="col-12 text-sm-end">
+                            <button className="btn btn-primary mb-0">Konfirmasi lamaran</button>
                         </div>
                     </form>
                 </div>
